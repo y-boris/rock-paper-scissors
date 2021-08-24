dd7322080f01e2833031e60318226229836dc2a6
böris <boris.yarmoulnik@gmail.com>
2021-08-25 02:23:58 +0300

    feat: finish all screen game / script

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-const toggle = document.querySelector(".button"),
-      modal = document.querySelector(".modal"),
-      close = document.querySelector(".modal__dialog-button")
+const btnShowModal = document.querySelector(".button"),
+      btnCloseModal = document.querySelector(".modal__dialog-button"),
+      modal = document.querySelector(".modal")
 
-toggle.addEventListener("click", function() {
-  modal.classList.add("modal-open")
-})
+// show / hide the rules modal
+function toggleModal() {
+  modal.classList.toggle("modal-open")
+}
 
-close.addEventListener("click", function() {
-  modal.classList.remove("modal-open")
-})
+// hide the rules when user clicks outside the modal window
+function windowOnClick(event) {
+  if(event.target === modal){
+      toggleModal()
+  }
+}
+
+// add function when the buttons/window are clicked
+btnShowModal.addEventListener("click", toggleModal)
+btnCloseModal.addEventListener("click", toggleModal)
+window.addEventListener('click', windowOnClick)
+
+////////// GAME //////////
+
+// create game elements
+const gameElements = {
+  gameButtons: Array.from(document.querySelector(".game__board").children)
+};
+
+let RESULT = 0
+
+console.log(gameElements.gameButtons)
+
+// play the round when button is clicked
+for (let elem of gameElements.gameButtons) {
+  elem.addEventListener("click", function() {
+    console.log("clicked")
+  });
+}
